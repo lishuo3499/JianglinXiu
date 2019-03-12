9d0c88f2397264b75b5976d121ce81938d811cbb
lishuo7854 <785483524@qq.com>
2019-03-13 01:59:09 +0800

    11

--- JianglinXiu/Blog/views.py
+++ JianglinXiu/Blog/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import os
 import time
-from django.core.urlresolvers import reverse
+# from django.urls import reverse
 # Create your views here.
 def index(request):
     blog = Atlas.objects.all()
